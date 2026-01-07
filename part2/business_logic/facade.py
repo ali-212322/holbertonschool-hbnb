f475542aea028ece38a2bc36a6fb8a36721949f1
root <root@ip-10-42-22-189.ec2.internal>
2026-01-07 18:37:16 +0000

    Implement Place endpoints with facade integration

--- part2/business_logic/facade.py
+++ part2/business_logic/facade.py
@@ -1,31 +1,29 @@
 #!/usr/bin/env python3
-"""
-HBnB Facade
-"""
 
+from persistence.in_memory_repository import InMemoryRepository
 from business_logic.user import User
 from business_logic.amenity import Amenity
-from persistence.in_memory_repository import InMemoryRepository
+from business_logic.place import Place
+from business_logic.review import Review
 
 
 class HBnBFacade:
     def __init__(self):
         self.user_repo = InMemoryRepository()
         self.amenity_repo = InMemoryRepository()
+        self.place_repo = InMemoryRepository()
+        self.review_repo = InMemoryRepository()
 
-    # =====================
-    # User methods
-    # =====================
     def create_user(self, **data):
         user = User(**data)
         self.user_repo.add(user)
         return user
 
-    def get_user(self, user_id):
-        return self.user_repo.get(user_id)
-
     def list_users(self):
         return self.user_repo.get_all()
+
+    def get_user(self, user_id):
+        return self.user_repo.get(user_id)
 
     def update_user(self, user_id, **data):
         user = self.get_user(user_id)
@@ -34,27 +32,55 @@ class HBnBFacade:
         user.update(**data)
         return user
 
-    # =====================
-    # Amenity methods
-    # =====================
-    def create_amenity(self, name):
-        amenity = Amenity(name=name)
+    def create_amenity(self, **data):
+        amenity = Amenity(**data)
         self.amenity_repo.add(amenity)
         return amenity
-
-    def get_amenity(self, amenity_id):
-        return self.amenity_repo.get(amenity_id)
 
     def list_amenities(self):
         return self.amenity_repo.get_all()
 
-    def update_amenity(self, amenity_id, name):
+    def get_amenity(self, amenity_id):
+        return self.amenity_repo.get(amenity_id)
+
+    def update_amenity(self, amenity_id, **data):
         amenity = self.get_amenity(amenity_id)
         if not amenity:
             return None
-        amenity.update(name=name)
+        amenity.update(**data)
         return amenity
 
+    def create_place(self, **data):
+        place = Place(**data)
+        self.place_repo.add(place)
+        return place
 
-# Singleton Facade instance
+    def list_places(self):
+        return self.place_repo.get_all()
+
+    def get_place(self, place_id):
+        return self.place_repo.get(place_id)
+
+    def update_place(self, place_id, **data):
+        place = self.get_place(place_id)
+        if not place:
+            return None
+        place.update(**data)
+        return place
+
+    def create_review(self, **data):
+        review = Review(**data)
+        self.review_repo.add(review)
+        return review
+
+    def list_reviews_by_place(self, place_id):
+        place = self.get_place(place_id)
+        if not place:
+            raise ValueError("Place not found")
+        return [
+            r for r in self.review_repo.get_all()
+            if r.place_id == place_id
+        ]
+
+
 facade = HBnBFacade()
