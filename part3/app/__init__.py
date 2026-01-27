cf21929becce2b9ebd8bf261af1e0cbc90537404
Ali Abdullah <aliabdullah1215@icloud.com>
2026-01-27 09:03:49 +0000

    Task 0: update application factory to support configuration

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -1,20 +1,9 @@
 from flask import Flask
-from flask_restx import Api
-from app.api.v1.users import api as users_ns
 
-def create_app():
+def create_app(config_class="config.DevelopmentConfig"):
+    """
+    Application Factory
+    """
     app = Flask(__name__)
-
-    api = Api(
-        app,
-        version='1.0',
-        title='HBnB API',
-        description='HBnB Application API'
-        doc='/api/v1/'
-    )
-
-    # Register namespaces
-    api.add_namespace(users_ns, path='/api/v1/users')
-
+    app.config.from_object(config_class)
     return app
-
