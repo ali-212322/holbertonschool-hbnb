2d21e552f71f78c2d863b97d649b3cff2391fabc
omar-hail <donz.h.h@gmail.com>
2026-01-30 01:10:40 +0300

    Refactor Amenity model to include relationships

--- part3/app/models/amenity.py
+++ part3/app/models/amenity.py
@@ -1,11 +1,22 @@
 #!/usr/bin/python3
 """Amenity model"""
 
+from app import db
 from app.models.base_model import BaseModel
-
+from app.models.place import place_amenity
+from sqlalchemy.orm import relationship
 
 class Amenity(BaseModel):
-    """Represents an amenity (e.g., Wi-Fi, Pool)"""
+    __tablename__ = "amenities"
+
+    name = db.Column(db.String(50), nullable=False, unique=True)
+
+    places = relationship(
+        "Place",
+        secondary=place_amenity,
+        back_populates="amenities",
+        lazy="subquery"
+    )
 
     def __init__(self, name):
         super().__init__()
@@ -16,4 +27,3 @@ class Amenity(BaseModel):
             raise ValueError("name must be at most 50 characters")
 
         self.name = name
-
