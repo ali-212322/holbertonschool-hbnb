1027bd58c9dce74a4784e5909e924fe236cb35cf
omar-hail <donz.h.h@gmail.com>
2026-01-29 15:18:24 +0300

    Refactor user retrieval by email
    Refactor get_user_by_email method to use get_by_attribute.

--- part3/app/services/repositories/user_repository.py
+++ part3/app/services/repositories/user_repository.py
@@ -9,4 +9,5 @@ class UserRepository(SQLAlchemyRepository):
         super().__init__(User)
 
     def get_user_by_email(self, email):
-        return self.model.query.filter_by(email=email).first()
+        """Retrieve a user by email."""
+        return self.get_by_attribute("email", email)
