fdb708bde0cb345fd106b00e9e5a0c2eee242eb1
omar-hail <donz.h.h@gmail.com>
2026-01-29 16:12:43 +0300

    Initialize SQLAlchemy in the Flask application
    Added SQLAlchemy initialization to the Flask app.

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
 
+db = SQLAlchemy()
 bcrypt = Bcrypt()
 jwt = JWTManager()
 
@@ -9,6 +11,8 @@ def create_app(config_class="config.DevelopmentConfig"):
     app = Flask(__name__)
     app.config.from_object(config_class)
 
+    # Initialize extensions
+    db.init_app(app)
     bcrypt.init_app(app)
     jwt.init_app(app)
 
