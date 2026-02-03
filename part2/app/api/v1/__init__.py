94e825d5c8298b5fe8bb04e6f444a0267058598d
Ali Summan <144675922+aliabdullah1215@users.noreply.github.com>
2026-02-03 19:19:46 +0300

    Update API namespaces to include paths

--- part2/app/api/v1/__init__.py
+++ part2/app/api/v1/__init__.py
@@ -1,17 +1,17 @@
 from flask import Blueprint
 from flask_restx import Api
 
-from app.api.v1.status import status_ns
-from app.api.v1.users import users_ns
-from app.api.v1.places import places_ns
-from app.api.v1.reviews import reviews_ns
-from app.api.v1.amenities import amenities_ns
+from app.api.v1.status import api as status_ns
+from app.api.v1.users import api as users_ns
+from app.api.v1.places import api as places_ns
+from app.api.v1.reviews import api as reviews_ns
+from app.api.v1.amenities import api as amenities_ns
 
 api_v1_bp = Blueprint("api_v1", __name__, url_prefix="/api/v1")
 api = Api(api_v1_bp)
 
-api.add_namespace(status_ns)
-api.add_namespace(users_ns)
-api.add_namespace(places_ns)
-api.add_namespace(reviews_ns)
-api.add_namespace(amenities_ns)
+api.add_namespace(status_ns, path="/status")
+api.add_namespace(users_ns, path="/users")
+api.add_namespace(places_ns, path="/places")
+api.add_namespace(reviews_ns, path="/reviews")
+api.add_namespace(amenities_ns, path="/amenities")
