d547c3cdd9a50d2be8ffce2ae8e007185f55d89f
Ali Summan <144675922+aliabdullah1215@users.noreply.github.com>
2026-02-03 18:55:41 +0300

    Import status_ns from app.api.v1.status

--- part2/app/__init__.py
+++ part2/app/__init__.py
@@ -4,6 +4,7 @@ from app.api.v1.users import api as users_ns
 from app.api.v1.amenities import api as amenities_ns
 from app.api.v1.places import api as places_ns
 from app.api.v1.reviews import api as reviews_ns
+from app.api.v1.status import api as status_ns
 
 def create_app():
     app = Flask(__name__)
