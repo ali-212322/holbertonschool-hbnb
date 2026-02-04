b61953d7e52768e4bf5dcada58bd019a4a43e19a
Ali Summan <144675922+aliabdullah1215@users.noreply.github.com>
2026-02-04 03:34:48 +0300

    Update __init__.py

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
 from flask_restx import Api # إضافة هذه المكتبة
+from app.api.v1.places import api as places_ns
 
 db = SQLAlchemy()
 bcrypt = Bcrypt()
@@ -29,5 +30,5 @@ def create_app(config_class="config.DevelopmentConfig"):
     api.add_namespace(users_ns, path='/api/v1/users')
     api.add_namespace(auth_ns, path='/api/v1/auth')
     api.add_namespace(amenities_ns, path='/api/v1/amenities')
-
+    api.add_namespace(places_ns, path='/api/v1/places')
     return app
